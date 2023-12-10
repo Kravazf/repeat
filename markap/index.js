19e29858b88ef37ee6691d8e81df9a8aee9ede50
Maks Kravchenko <123405337+Kravazf@users.noreply.github.com>
2023-12-11 01:54:40 +0200

    dz-2-str-num-done

---
+++ markap/index.js
@@ -0,0 +1,11 @@
+// let firstValue = prompt('Write 1 value');
+// let secondValue = prompt('Write 1 value');
+// let thirdValue = prompt('Write 1 value');
+
+// let concatenatedString = `${firstValue} ${secondValue} ${thirdValue}`;
+// console.log(concatenatedString);
+
+let userNumber = 12345;
+let userNumberToString = userNumber.toString();
+
+console.log(`${userNumberToString[0]} ${userNumberToString[1]} ${userNumberToString[2]} ${userNumberToString[3]} ${userNumberToString[4]}`);
