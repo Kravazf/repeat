4aa10deb93f946d30bdc69fe95628fe7818e391f
Maks Kravchenko <123405337+Kravazf@users.noreply.github.com>
2023-12-11 02:14:03 +0200

    string-numbers-done!

--- markap/index.js
+++ markap/index.js
@@ -5,7 +5,29 @@
 // let concatenatedString = `${firstValue} ${secondValue} ${thirdValue}`;
 // console.log(concatenatedString);
 
-let userNumber = 12345;
-let userNumberToString = userNumber.toString();
+// let userNumber = 12345;
+// let userNumberToString = userNumber.toString();
 
-console.log(`${userNumberToString[0]} ${userNumberToString[1]} ${userNumberToString[2]} ${userNumberToString[3]} ${userNumberToString[4]}`);
+// console.log(`
+// ${userNumberToString[0]}
+// ${userNumberToString[1]}
+// ${userNumberToString[2]}
+// ${userNumberToString[3]}
+// ${userNumberToString[4]}
+// `);
+
+// let firstUserNum = +prompt('Write first number');
+// let secondUserNum = +prompt('Write second number');
+
+// let addition = firstUserNum + secondUserNum;
+// let subtract = firstUserNum - secondUserNum;
+// let division = firstUserNum / secondUserNum;
+// let multiplication = firstUserNum * secondUserNum;
+
+// console.log(
+//   `${firstUserNum} + ${firstUserNum} = ${addition}\n${firstUserNum} - ${firstUserNum} = ${subtract}\n${firstUserNum} / ${firstUserNum} = ${division}\n${firstUserNum} * ${firstUserNum} = ${multiplication}`);
+
+let hours = parseInt(prompt('How many hours?'));
+
+let hoursToSeconds = hours * 3600;
+console.log(`${hoursToSeconds} s!`);
